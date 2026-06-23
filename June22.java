8bdc5aa30f18f7bbce770c73561223221170450d
purush2300 <reddypurushothm@gmail.com>
2026-06-23 21:11:39 +0530

    Initial commit

--- June22.java
+++ June22.java
@@ -1,11 +1,11 @@
 public class June22 {
     public static void main(String[] args) {
-        int[]a={2,1,1,5,8,5};
+        int[]a={2,1,1,5,8,5,4};
         int sum=4;
         int cnt=0;
-        for(int i=0;i<a.length-1;i++){
+        for(int i=0;i<a.length;i++){
             
-            for(int j=i+1;j<a.length-1;j++){
+            for(int j=i+1;j<a.length;j++){
                 if(a[i]+a[j]<=sum){
                     cnt++;
                 }
---
+++ June23.java
@@ -0,0 +1,21 @@
+public class June23 {
+    public static void main(String[] args) {
+        int[]arr={1,1,2,2,1,1};
+        int target=4;
+        int len=0;
+        int sum=0;
+        
+       for(int i=0,j=0;j<arr.length;j++){
+            sum+=arr[j];
+
+            while(sum>target){
+                sum-=arr[i];
+                i++;
+            }
+            
+                len+=(j-i+1);
+            
+       }
+    System.out.println(len);
+    }
+}
