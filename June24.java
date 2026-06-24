1eede7aefc21f2ffde0ac59a73824a39b36607d9
purush2300 <reddypurushothm@gmail.com>
2026-06-24 20:47:24 +0530

    Initial commit

---
+++ June24.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class June24 {
+    public static void main(String[] args) {
+        int[]arr={1, 1, 2, 1, 3, 5};
+        HashMap <Integer,Integer> map=new HashMap<>();
+        int cnt=0;
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i],map.getOrDefault(arr[i], 0)+1);
+            if(map.get(arr[i])>arr.length/2){
+                 System.out.println(arr[i]);
+                 return;
+            }
+        }
+        System.out.println(-1);
+    }
+}
