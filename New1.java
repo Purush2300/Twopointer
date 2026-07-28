90a81b83f72115b41a77618c91acdef207f35fef
purush2300 <reddypurushothm@gmail.com>
2026-07-28 20:43:22 +0530

    Initial commit

--- New1.java
+++ New1.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class New1 {
     public static void main(String[] args) {
-        int []a={1,3,3,4,2};
+        int []a={1,3,3,4,1};
         Arrays.sort(a);
         for (int i = 1; i < a.length; i++) {
             if(a[i]==a[i-1]){
